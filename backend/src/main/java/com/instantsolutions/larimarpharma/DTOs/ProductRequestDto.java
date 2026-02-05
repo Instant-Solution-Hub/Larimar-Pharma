87c332523ef1711348778558c11d8d9e6f180e99
naveen-kk4 <naveennig2001@gmail.com>
2026-02-05 15:13:52 +0530

    Product for Admin done

--- backend/src/main/java/com/instantsolutions/larimarpharma/DTOs/ProductRequestDto.java
+++ backend/src/main/java/com/instantsolutions/larimarpharma/DTOs/ProductRequestDto.java
@@ -1,5 +1,8 @@
 package com.instantsolutions.larimarpharma.DTOs;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Data
@@ -8,11 +11,24 @@ import lombok.*;
 @Builder
 public class ProductRequestDto {
 
+    @NotBlank(message = "Product name is required")
     private String name;
+
+    @NotBlank(message = "Product category is required")
     private String category;
+
+    @NotBlank(message = "Product description is required")
+    @Size(max = 40 , message = "Limit is 40 letters")
     private String description;
+
+    @NotNull(message = "Product price is required")
     private Double price;
+
+    @NotNull(message = "Product pts is required")
     private Double pts;
+
+    @NotNull(message = "Product ptr is required")
     private Double ptr;
+
     private boolean active;
 }
