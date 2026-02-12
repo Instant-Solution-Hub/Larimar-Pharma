31d7a66fe024a1f3c7f4ec341d442fe1a7197362
naveen-kk4 <naveennig2001@gmail.com>
2026-02-12 13:44:10 +0530

    CORS done

--- backend/src/main/java/com/instantsolutions/larimarpharma/config/WebConfig.java
+++ backend/src/main/java/com/instantsolutions/larimarpharma/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.instantsolutions.larimarpharma.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -8,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
